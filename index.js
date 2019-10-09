2a5da42f631d0214c3428901a57e27172dae1296
cyber-eternal <92edgarsargsyan@gmail.com>
2019-10-09 10:16:16 +0400

    added more functionality

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 "use strict";
 
-var _ = require('lodash');
+var omitBy = require('lodash/omitBy');
+var isNil = require('lodash/isNil');
+var forEach = require('lodash/forEach');
 var AWS = require('aws-sdk');
-// var aws = new AWS;
 var stage = process.env.STAGE;
-
-// var config = ({ region, accessKeyId, secretAccessKey }) => {
-//   try {
-//     return AWS.Config({
-//       accessKeyId, secretAccessKey, region
-//     });
-//   } catch (error) {
-//     return error
-//   }
-// };
 
 var call = (action, params) => {
   return new Promise((resolve, reject) => {
@@ -64,7 +55,7 @@ var getItemByGSI = ({
       filter && filterValue && operator && filter1 && filterValue1 ? (params.FilterExpression += ` ${operator} #${filter1} = :${filter1}`) &&
         (params.ExpressionAttributeNames[`#${filter1}`] = filter1) &&
         (params.ExpressionAttributeValues[`:${filter1}`] = filterValue1) : '';
-      params = _.omitBy(params, _.isNil);
+      params = omitBy(params, isNil);
       var result = await call('query', params);
       resolve(result);
     } catch (error) {
@@ -73,112 +64,152 @@ var getItemByGSI = ({
   })
 };
 
-// var writeDataIntoDB = (TableName, Item) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       var params = {
-//         TableName: `${TableName}-${stage}`,
-//         Item
-//       };
-//       await call('put', params);
-//       resolve('Success');
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// };
+var writeDataIntoDB = (TableName, Item) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var params = {
+        TableName: `${TableName}-${stage}`,
+        Item
+      };
+      await call('put', params);
+      resolve('Success');
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
-// var getAllItemsByGSI = (data) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       let finalData = [];
-//       let gettingData = await getItemByGSI(data);
-//       console.log('gettingData', gettingData);
-//       finalData = finalData.concat(gettingData.Items);
-//       if (gettingData.LastEvaluatedKey) {
-//         let final2 = await getAllItemsByGSI({ ...data, LastEvaluatedKey: gettingData.LastEvaluatedKey });
-//         finalData = finalData.concat(final2);
-//       }
-//       resolve(finalData);
-//     } catch (err) {
-//       reject(err);
-//     }
-//   });
-// };
+var getAllItemsByGSI = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // {
+      //   TableName,
+      //   IndexName,
+      //   attribute,
+      //   value,
+      //   sortKey,
+      //   sortValue,
+      //   filter,
+      //   filterValue,
+      //   operator,
+      //   filter1,
+      //   filterValue1,
+      //   LastEvaluatedKey,
+      //   ScanIndexForward,
+      //   Limit
+      // }
+      var finalData = [];
+      var gettingData = await getItemByGSI(data);
+      finalData = finalData.concat(gettingData.Items);
+      if (gettingData.LastEvaluatedKey) {
+        var final2 = await getAllItemsByGSI({ ...data, LastEvaluatedKey: gettingData.LastEvaluatedKey });
+        finalData = finalData.concat(final2);
+      }
+      resolve(finalData);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-// var updateInDB = ({ TableName, Key, updatedData }) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       console.log('updatedData', updatedData);
-//       var params = {
-//         TableName: `${TableName}-${stage}`,
-//         Key,
-//         UpdateExpression: 'SET #updatedAt = :updatedAt',
-//         ExpressionAttributeValues: {
-//           ':updatedAt': Date.now()
-//         },
-//         ExpressionAttributeNames: {
-//           '#updatedAt': 'updatedAt'
-//         },
-//       };
-//       _.forEach(updatedData, (item, key) => {
-//         if (key !== 'id' && key !== 'provider') {
-//           if (typeof item === 'string') {
-//             params.UpdateExpression += `, #${key} = :${key}`;
-//             params.ExpressionAttributeValues[`:${key}`] = item;
-//             params.ExpressionAttributeNames[`#${key}`] = key;
-//           } else if (typeof item === 'object' && Object.keys(item).length > 0) {
-//             _.forEach(item, (item1, key2) => {
-//               params.UpdateExpression += `, ${key}.#${key2} = :${key2}`;
-//               params.ExpressionAttributeValues[`:${key2}`] = item1;
-//               params.ExpressionAttributeNames[`#${key2}`] = key2;
-//             });
-//           }
-//         }
-//       });
-//       await call('update', params);
-//       resolve('Success');
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// };
+var updateInDB = ({ TableName, Key, updatedData }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log('updatedData', updatedData);
+      var params = {
+        TableName: `${TableName}-${stage}`,
+        Key,
+        UpdateExpression: 'SET #updatedAt = :updatedAt',
+        ExpressionAttributeValues: {
+          ':updatedAt': Date.now()
+        },
+        ExpressionAttributeNames: {
+          '#updatedAt': 'updatedAt'
+        },
+      };
+      forEach(updatedData, (item, key) => {
+        if (key !== 'id' && key !== 'provider') {
+          if (typeof item === 'string') {
+            params.UpdateExpression += `, #${key} = :${key}`;
+            params.ExpressionAttributeValues[`:${key}`] = item;
+            params.ExpressionAttributeNames[`#${key}`] = key;
+          } else if (typeof item === 'object' && Object.keys(item).length > 0) {
+            forEach(item, (item1, key2) => {
+              params.UpdateExpression += `, ${key}.#${key2} = :${key2}`;
+              params.ExpressionAttributeValues[`:${key2}`] = item1;
+              params.ExpressionAttributeNames[`#${key2}`] = key2;
+            });
+          }
+        }
+      });
+      await call('update', params);
+      resolve('Success');
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
-// var deleteAttributes = ({ TableName, Key, attributesList }) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       let params = {
-//         TableName: `${TableName}-${stage}`,
-//         Key,
-//       };
-//       if (attributesList.length) {
-//         params.UpdateExpression = '';
-//         params.ExpressionAttributeNames = {};
-//         let isFirst = true;
-//         for (const attribute of attributesList) {
-//           attribute !== attributesList[0] ? isFirst = false : '';
-//           params.UpdateExpression += isFirst ? `#${attribute}` : `,#${attribute}`;
-//           params.ExpressionAttributeNames[`#${attribute}`] = attribute;
-//         }
-//         console.log('params', params);
-//         await dynamoDbLib.call('update', params);
-//         console.log('Attributes successfully removed');
-//         resolve('Attributes successfully removed');
-//       } else {
-//         console.log('Nothing to remove');
-//         resolve('Nothing to remove');
-//       }
-//     } catch (error) {
-//       console.log('ERROR in deleteAttributes', error);
-//       reject(error);
-//     }
-//   });
+var removeAttributes = ({ TableName, Key, attributesList }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let params = {
+        TableName: `${TableName}-${stage}`,
+        Key,
+      };
+      if (attributesList.length) {
+        params.UpdateExpression = 'REMOVE ';
+        params.ExpressionAttributeNames = {};
+        for (const attribute of attributesList) {
+          var hasNext = attributesList[attributesList.indexOf(attribute) + 1] ? true : false
+          params.UpdateExpression += hasNext ? `#${attribute}` : `, #${attribute}`;
+          params.ExpressionAttributeNames[`#${attribute}`] = attribute;
+        }
+        await dynamoDbLib.call('update', params);
+        resolve('Attributes successfully removed');
+      } else {
+        console.log('Nothing to remove');
+        resolve('Nothing to remove');
+      }
+    } catch (error) {
+      console.log('ERROR in deleteAttributes', error);
+      reject(error);
+    }
+  });
+};
+
+const deleteItem = ({ TableName, Key }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var params = {
+        TableName,
+        Key
+      };
+      await call('delete', params);
+      resolve(`Item ${JSON.stringify(Key)} from ${TableName} successfully deleted`)
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+// var config = ({ region, accessKeyId, secretAccessKey }) => {
+//   try {
+//     return AWS.Config({
+//       accessKeyId, secretAccessKey, region
+//     });
+//   } catch (error) {
+//     return error
+//   }
 // };
 
 module.exports = {
   // config,
   getItemByGSI,
-  // getAllItemsByGSI,
-  // updateInDB,
-  // writeDataIntoDB
+  getAllItemsByGSI,
+  updateInDB,
+  writeDataIntoDB,
+  removeAttributes,
+  deleteItems,
+  deleteItem
 };
